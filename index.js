3a775a2f013ecc927878d51fea912b6279f1a53f
ropsutius <ropsutius@gmail.com>
2022-11-25 14:34:58 +0200

    Added ESLint

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 
-morgan.token('person', (req, res) => {
+morgan.token('person', (req) => {
   console.log(req.body);
   if (
     req.body &&
